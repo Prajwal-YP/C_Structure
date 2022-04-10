4865728068be7c08f3808f385ed0d10770d0b2ab
Prajwal_Y_P <87578946+Prajwal-YP@users.noreply.github.com>
2022-04-10 11:25:29 +0530

    Create unistr1.c

---
+++ unistr1.c
@@ -0,0 +1,112 @@
+/*
+	Program Name	:	unistr1.c
+	Author			:	Prajwal Y p
+	Task			: 	To understand concept of Union-structure
+*/
+
+#include<stdio.h>
+#pragma pack(1)
+
+typedef struct 
+{
+	int rollno;
+	char name[30];
+	int m;
+	union
+	{
+		struct
+		{
+			int p,ch;
+		}sci;
+		struct
+		{
+			int e,co;
+		}com;
+	}marks;
+}stu;
+
+
+main()
+{
+	void read(stu*,int*,int);
+	void disp(stu*,int[],int);
+	
+	int n;
+	printf("\nEnter student size : ");
+	scanf("%d",&n);
+	stu s[n];
+	int c[n];
+	
+	read(s,c,n);
+	
+	disp(s,c,n);
+	
+	printf("\n\n------------------------------------------------------\n\n");
+}
+
+void read(stu *s,int *c,int n)
+{
+	int i;
+	printf("\nSTUDENT DETAILS ENTRY :");
+	printf("\n---------------------\n");
+	
+	for(i=0;i<n;i++)
+	{
+		printf("\nEnter Student roll no\t: ");
+		scanf("%d",&(s+i)->rollno);
+		printf("Enter Student Name\t: ");
+		fflush(stdin);
+		gets((s+i)->name);
+		printf("\nCaterory list\n\t1.Science\n\t2.Commerce\nEnter choice : ");
+		scanf("%d",(c+i));
+		while(*(c+i)<1 || *(c+i)>2)
+		{
+			printf("\aEnter Valid choice : ");
+			scanf("%d",(c+i));
+		}
+		printf("\nEnter maths marks\t: ");
+		scanf("%d",&(s+i)->m);
+		if(*(c+i)==1)
+		{
+			printf("\nEnter physics marks\t: ");
+			scanf("%d",&(s+i)->marks.sci.p);
+			printf("Enter chemistry marks\t: ");
+			scanf("%d",&(s+i)->marks.sci.ch);
+		}
+		else
+		{
+			printf("\nEnter economics marks\t: ");
+			scanf("%d",&(s+i)->marks.com.e);
+			printf("Enter commerce marks\t: ");
+			scanf("%d",&(s+i)->marks.com.co);
+		}
+		printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+	}
+}
+
+void disp(stu *s,int c[],int n)
+{
+	int i;
+	
+	for(i=0;i<n;i++)
+	{
+		printf("\n\nStudent-%d Details",i+1);
+		printf("\n------------------\n");
+		
+		printf("\nStudent roll no\t\t: %d",(s+i)->rollno);
+		printf("\nEnter Student Name\t: %s",(s+i)->name);
+		printf("\nStudent maths marks\t: %d",(s+i)->m);
+		if(c[i]== 1)
+		{
+			printf("\nStudent physics marks\t: %d",(s+i)->marks.sci.p);
+			printf("\nStudent chemistry marks\t: %d",(s+i)->marks.sci.ch);
+		}
+		else
+		{
+			printf("\nStudent economics marks\t: %d",(s+i)->marks.com.e);
+			printf("\nStudent commerce marks\t: %d",(s+i)->marks.com.co);
+		}
+		
+	}
+	
+}
